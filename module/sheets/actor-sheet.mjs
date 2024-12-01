66daaab47d0433cef3f2e1af7e91e5f6fb535873
SoFire <sophia.hirschwitz@gmail.com>
2024-12-01 14:52:39 +0100

    first test for karma math

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -190,6 +190,9 @@ export class edrpgSystemActorSheet extends ActorSheet {
     // Rollable abilities.
     html.on('click', '.rollable', this._onRoll.bind(this));
 
+    // Use Karma abilities.
+    html.on('click', '.use_karma', this._onUseKarma.bind(this));
+
     // Drag events for macros.
     if (this.actor.isOwner) {
       let handler = (ev) => this._onDragStart(ev);
@@ -257,6 +260,26 @@ export class edrpgSystemActorSheet extends ActorSheet {
         rollMode: game.settings.get('core', 'rollMode'),
       });
       return roll;
+    }
+  }
+
+  _onUseKarma(event) {
+    event.preventDefault();
+    const element = event.currentTarget;
+    const dataset = element.dataset;
+
+    const itemId = element.closest('.item').dataset.itemId;
+    const item = this.actor.items.get(itemId);
+
+    if (this.actor.karma.value >= item.karma_cost){
+      if (item.roll.diceNum > 0){
+        console.log("hier müsste was gwürfelt werden");
+      }else{
+        console.log("hier nicht");
+      }
+      this.actor.karma.value = this.actor.karma.value - item.karma_cost; 
+    }else{
+      console.log("nicht genug karma");
     }
   }
 }
